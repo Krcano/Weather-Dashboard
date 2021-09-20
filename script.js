88845d82a5ecc8cf5389398017b41273192a12b1
Kelly R Cano <krcano2001@gmail.com>
2021-09-20 11:26:24 -0800

    added render city function

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-var repoList = document.querySelector('ul');
+
 var fetchButton = document.getElementById('fetch-button');
+var cityListContainer = document.querySelector(".cityListContainer")
+var searchButton = document.querySelector(".citySearch")
+var cityInput = document.querySelector(".cityInput")
+var city = [];
  
-//  basic set up need to input own variables
- fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          var listItem = document.createElement('li');
-          listItem.textContent = data[i];
-          ingredientsList.appendChild(listItem);
-        }
-        console.log(response)
-      });
+// //  basic set up need to input own variables
+//  fetch(requestUrl)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         for (var i = 0; i < data.length; i++) {
+//           var listItem = document.createElement('li');
+//           listItem.textContent = data[i];
+//           ingredientsList.appendChild(listItem);
+//         }
+//         console.log(response)
+//       });
+
+
+      // The following function renders items in a todo list as <li> elements
+function renderCity() {
+  
+
+  cityList.innerHTML ="";
+
+  for (let i = 0; i < city.length; i++) {
+    
+    var cityList= city[i]
+    
+    var li = document.createElement('li');
+    li.textContent = cityList
+    
+    cityListContainer.appendChild(li);
+    
+}
+
+ }
+ 
+searchButton.addEventListener("click", function(){
+  var cityText = cityInput.value;
+  city.push(cityText);
+  cityInput.value = " ";
+  renderWeather();
+});
   
